1acca6a8cde5fc3d039f036a1fd041f63499772f
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-17 19:30:48 -0500

    added first route which shows navbar and home page with exact path

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-import Login from '../Login/Login';
-import Register from '../Register/Register';
-import Dashboard from '../Dashboard/Dashboard';
+// import Login from '../Login/Login';
+// import Register from '../Register/Register';
+// import Dashboard from '../Dashboard/Dashboard';
 import Home from '../Home/Home';
 import Navbar from '../Navbar/Navbar';
-import Game from '../Game/Game';
-import Scripture from '../VerseSpa/VerseSpa';
+// import Game from '../Game/Game';
+// import Scripture from '../VerseSpa/VerseSpa';
 
 import './App.css';
 
@@ -18,13 +19,16 @@ class App extends Component {
   render () {
     return (
       <div className="App container">
-        <Login />
-        <Register />
-        <Dashboard />
-        <Home />
-        <Navbar />
-        <Game />
-        <Scripture />
+        <BrowserRouter>
+          <div>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route path="/home" exact component={Home}/>
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
