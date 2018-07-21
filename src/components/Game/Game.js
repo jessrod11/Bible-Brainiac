8ee5b8b525005150f80f194226375e7c0bbd8c61
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-21 13:29:38 -0500

    verses printing to the page in h1 tag

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import scriptureRequests from '../../FirebaseRequests/scriptures';
+
 import './Game.css';
 
 class Game extends React.Component {
+  state = {
+    scriptures: [],
+  }
+
+  componentDidMount () {
+    scriptureRequests
+      .getRequest()
+      .then((scriptures) => {
+        this.setState({scriptures});
+      })
+      .catch((err) => {
+        console.error('error in scriptureGetRequest', err);
+      });
+  }
+
   render () {
+    const scriptureComponents = this.state.scriptures.map((scripture) => {
+      return (
+        <h1>{scripture.verse}</h1>
+      );
+    });
+
     return (
-      <div className="Game">
+      <div className="Game ">
         <h1>Game</h1>
+        {scriptureComponents}
       </div>
     );
-  }
-}
+  };
+};
 
 export default Game;
