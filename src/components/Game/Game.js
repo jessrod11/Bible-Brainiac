283cc59d1b4f6f527fcb9db076a10d9e89518658
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-23 20:06:47 -0500

    fixed home route

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -22,15 +22,16 @@ class Game extends React.Component {
   render () {
     const scriptureComponents = this.state.scriptures.map((scripture) => {
       return (
-        <div className="col-md-4">
+        <div className="col-md-4 selected-Card">
           <h1>{scripture.verse}</h1>
         </div>
       );
     });
 
     return (
-      <div className="Game ">
+      <div className="Game">
         <h1>Game</h1>
+        <button className="btn btn-lg btn-info" onClick={this.correctVerseEvent}>Gen 1:1</button>
         {scriptureComponents}
       </div>
     );
