25473ebdfc72a4bbd3a46f365e715ee9d0c2ce5a
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-27 16:05:52 -0500

    getting faves

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import favesRequests from '../../FirebaseRequests/faves';
+// import authRequests from '../../FirebaseRequests/auth';
+// import Faves from '../Faves/Faves';
+
 import './Dashboard.css';
 
 class Dashboard extends React.Component {
+  state = {
+    faves: [],
+  }
+
+  componentDidMount () {
+    favesRequests.getRequest()
+      .then((faves) => {
+        this.setState({ faves });
+      })
+      .catch((err) => {
+        console.error('error while getting faves', err);
+      });
+  }
   render () {
+    const favesComponents = this.state.faves.map((fave) => {
+      return (
+        <div>
+          <h3>{fave.verse}</h3> <button className="glyphicon glyphicon-heart"></button> <button className="glyphicon glyphicon-trash"></button>
+          <h5>{fave.verseBook}</h5>
+        </div>
+      );
+    });
+
     return (
       <div className="Dashboard">
-        <h1>Dashboard</h1>
-        <button className="btn btn-danger">Start New Game</button>
+        <div className="col-md-6">
+          <h1>Dashboard</h1>
+          <button className="btn btn-danger">Start New Game</button>
+        </div>
+        <div className="col-md-6">
+          <h1>My Favorite Scriptures</h1>
+          {favesComponents}
+        </div>
       </div>
     );
   }
---
+++ src/components/Faves/Faves.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Faves.css';
+
+class Faves extends React.Component {
+  render () {
+    return (
+      <div className="Faves">
+        <h1>My Favorite Scriptures</h1>
+      </div>
+    );
+  }
+}
+
+export default Faves;
