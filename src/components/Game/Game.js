f7c0ff464b803e760fac78034ac10c8c32d7c369
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-28 11:07:50 -0500

    added object.keys to grab data

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,70 +1,47 @@
 import React from 'react';
 import Scripture from '../Scripture/Scripture';
-import scriptureRequests from '../../FirebaseRequests/scriptures';
+// import scriptureRequests from '../../FirebaseRequests/scriptures';
 import gameRequests from '../../FirebaseRequests/games';
-import authRequests from '../../FirebaseRequests/auth';
+// import authRequests from '../../FirebaseRequests/auth';
 
 import './Game.css';
 
 class Game extends React.Component {
   state = {
-    scriptures: [],
-    newGame: {
-      scriptures: {
-        card1: '',
-        card2: '',
-        card3: '',
-        card4: '',
-        card5: '',
-        card6: '',
-        card7: '',
-        card8: '',
-      },
-      correctCard: '',
-      inProgress: true,
-      createdAt: '',
-      completedAt: '',
-      uid: '',
-    },
+    game: {},
   }
 
-  grabNewGame = () => {
-    gameRequests.getRequest(authRequests.getUID())
-      .then((newGame) => {
-        console.error('what is new game', newGame);
+  componentDidMount () {
+    const gameId = this.props.match.params.id;
+    gameRequests.getSingleGameRequest(gameId)
+      .then((game) => {
+        this.setState({ game });
       })
       .catch((err) => {
         console.error('error while getting game', err);
       });
   }
 
-  componentDidMount () {
-    scriptureRequests
-      .getRequest()
-      .then((scriptures) => {
-        this.setState({scriptures});
-      })
-      .catch((err) => {
-        console.error('error in scriptureGetRequest', err);
-      });
-  }
-
   render () {
-    const scriptureComponents = this.state.scriptures.map((scripture) => {
-      return (
-        <Scripture
-          key={scripture.id}
-          details={scripture}
-          grabNewGame={this.grabNewGame}
-        />
-      );
-    });
+    let gameComponents = null;
+    if (this.state.game.scriptures) {
+      gameComponents = Object.keys(this.state.game.scriptures).map((cardId) => {
+        const details = this.state.game.scriptures[cardId].verse;
+        const verseId = this.state.game.scriptures[cardId].id;
+        return (
+          <Scripture
+            key={verseId}
+            details={details}
+          />
+        );
+      });
+    }
 
     return (
       <div className="Game">
         <h1>Game</h1>
-        <button className="btn btn-lg btn-danger">Gen 1:1</button>
-        {scriptureComponents}
+        <button className="btn btn-lg btn-danger">{this.state.game.correctCard}</button>
+        {gameComponents}
       </div>
     );
   };
