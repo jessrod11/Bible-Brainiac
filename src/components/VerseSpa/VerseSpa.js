0ef61e207d856a021cc22625a1b2b06f9e5ad411
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-07-30 17:22:33 -0500

    hardcoded scripture

--- src/components/VerseSpa/VerseSpa.js
+++ src/components/VerseSpa/VerseSpa.js
@@ -5,9 +5,19 @@ import './VerseSpa.css';
 class VerseSpa extends React.Component {
 
   render () {
+    // const {details} = this.props;
     return (
       <div className="VerseSpa">
         <h1>Scripture for the week</h1>
+        <div>
+          <h1>"For I know the plans I have for you, declares the Lord, plans to prosper you and not harm you, plans to give you hope and a future."</h1>
+          <h3> Jeremiah 29:11</h3>
+        </div>
+        <button
+          className=" btn btn-lg glyphicon glyphicon-heart"
+        >
+        </button>
+        <button className="btn btn-lg btn-info">Complete</button>
       </div>
     );
   }
