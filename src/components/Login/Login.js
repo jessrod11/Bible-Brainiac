273b19a0f57075baf02c77cbf069a2918cf4f1d8
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-08-13 21:27:13 -0500

    updated login form and added styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -39,59 +39,43 @@ class Login extends React.Component {
   };
 
   render () {
+    const userImage = require('./Image/user.png');
     const { user } = this.state;
     return (
       <div className="Login">
-        <div id="login-form">
-          <h1 className="text-center">Login</h1>
-          <form className="form-horizontal col-sm-4 col-m-offset-4 col-xs-8 col-xs-offset-2 text-center">
-            <div className="form-group">
-              <label htmlFor="inputEmail" className="col-sm-2 control-label">
-                Email
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="email"
-                  className="form-control"
-                  id="inputEmail"
-                  placeholder="Email..."
-                  value={user.email}
-                  onChange={this.emailChange}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="inputPassword" className="col-sm-2 control-label">
-                Password
-              </label>
-              <div className="col-sm-10">
-                <input
-                  type="password"
-                  className="form-control"
-                  id="inputPassword"
-                  placeholder="Password..."
-                  value={user.password}
-                  onChange={this.passwordChange}
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-offset-2 col-m-10 text-center">
-                <Link to="/register">Need to register?</Link>
-              </div>
-            </div>
-            <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-10">
-                <button
-                  type="submit"
-                  className="btn btn-default col-xs-12"
-                  onClick={this.loginClickEvent}>
-                  Login
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
+        <img src={userImage} className="user" alt="login avatar"/>
+        <h2 className="text-center">Login</h2>
+        <form>
+          <label htmlFor="inputEmail">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="inputEmail"
+            placeholder="Enter Email..."
+            value={user.email}
+            onChange={this.emailChange}
+          />
+          <label htmlFor="inputPassword">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="inputPassword"
+            placeholder="Enter Password..."
+            value={user.password}
+            onChange={this.passwordChange}
+          />
+          <Link to="/register" className="registerLink">Need to register?</Link>
+          <button
+            type="submit"
+            className="btn btn-default btn-lg changeButton"
+            onClick={this.loginClickEvent}>
+            Login
+          </button>
+        </form>
       </div>
     );
   }
